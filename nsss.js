6fb51529909448fa0cf62f419a337f0b348c2d87
Jesús Leganés Combarro "Piranna" <piranna@gmail.com>
2013-07-24 22:07:08 +0200

    Using objects for messages

--- nsss.js
+++ nsss.js
@@ -7,19 +7,23 @@ function NSSS(socket, uid, room)
 {
   var self = this;
 
-  var requestID = 0;
-
   /**
    * UUID generator
    */
   var UUIDv4 = function b(a){return a?(a^Math.random()*16>>a/4).toString(16):([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g,b)};
 
-  uid = uid || UUIDv4();
+
+  uid  = uid  || UUIDv4();
+  room = room || '');
+
+  var requestID = 0;
+
 
   function send(data)
   {
     socket.send(JSON.stringify(data));
   }
+
 
   socket.onopen = function()
   {
@@ -28,7 +32,9 @@ function NSSS(socket, uid, room)
     {
       event = JSON.parse(event.data);
 
-      self.dispatchEvent(event);
+      var event2 = new Event(event.method);
+
+      self.dispatchEvent(event2);
     };
 
     // Send our UID
@@ -45,26 +51,66 @@ function NSSS(socket, uid, room)
   };
 
 
+  function createNotification(method, params)
+  {
+    var notification =
+    {
+      jsonrpc: "3.0",
+      method:  method
+    };
+
+    if(params)
+    {
+      if(params.length == 1 && params[0] instanceof object)
+        params = params[0];
+
+      notification.params = params
+    }
+
+    return notification
+  }
+
+  function createRequest(method, params)
+  {
+    var request = createNotification(method, params)
+
+    request.id = requestID++
+
+    return request
+  }
+
+
   /**
    * Compose and send message
    * @param message Data to be send
    * @param {String|undefined} uid Identifier of the remote peer. If null,
    * message is send by broadcast to all connected peers
    */
-  this.call = function(type, to)
+  this.call = function(method, dest)
   {
-    var message =
-    {
-      type: type,
-      to:   to,
-      id:   requestID++
-    };
+    var request = createRequest(method, Array.prototype.slice.call(arguments, 2)
 
-    var args = Array.prototype.slice.call(arguments, 2);
-    if(args)
-      message.args = args
+    request.dest = dest;
 
-    send(message);
+    send(request);
+  };
+
+  /**
+   * Compose and send message
+   * @param message Data to be send
+   * @param {String|undefined} uid Identifier of the remote peer. If null,
+   * message is send by broadcast to all connected peers
+   */
+  this.callBroadcast = function(method, room)
+  {
+    var request = createRequest(method, Array.prototype.slice.call(arguments, 2)
+
+    if(room != undefined
+    && room != null
+    && room != '')
+      request.room = room
+
+    send(request);
   };
 
 
